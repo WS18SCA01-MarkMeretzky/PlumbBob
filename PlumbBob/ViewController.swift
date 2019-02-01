ed396514e11005faa2212f2f3ccf922811896d20
Mark Meretzky <mark.meretzky@gmail.com>
2019-02-01 09:29:11 -0500

    Commit.

--- PlumbBob/ViewController.swift
+++ PlumbBob/ViewController.swift
@@ -6,15 +6,39 @@
 //  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PlumbBobDelegate {
+
+    let plumbBob: PlumbBob = PlumbBob();
+    @IBOutlet weak var arrowLabel: UILabel!;
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //The plumbBob object will call the angleUpdated(to:) method of some object.
+        //Which object will it be?
+        plumbBob.delegate = self;   //self is the ViewController object.
     }
-
-
+    
+    //Turn on the plumbBob object at the last possible moment.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        plumbBob.turnOn();
+    }
+    
+    //Turn off the plumbBob object at the first possible moment.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        plumbBob.turnOff();
+    }
+    
+    //Called by the PlumbBob object whenever it has a new angle.
+    
+    func angleUpdated(to angle: CGFloat) {
+        //Rotate the arrow so it points straight down.
+        arrowLabel.transform = CGAffineTransform(rotationAngle: angle);
+    }
+    
 }
-
